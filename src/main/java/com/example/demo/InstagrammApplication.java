ea9673738ba8931e25da66320b9fc2d2ea26857d
neiton <neitonmatisso@gmail.com>
2022-05-02 14:50:20 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/InstagrammApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagrammApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagrammApplication.class, args);
+	}
+
+}
